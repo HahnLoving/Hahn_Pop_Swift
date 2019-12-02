fe616b89e2f16d49844b1dc0179b64dc80a03c12
HahnLoving <z253154650@qq.com>
2019-12-02 10:22:43 +0800

    Initial Commit

---
+++ Hahn_Pop_SwiftTests/Hahn_Pop_SwiftTests.swift
@@ -0,0 +1,34 @@
+//
+//  Hahn_Pop_SwiftTests.swift
+//  Hahn_Pop_SwiftTests
+//
+//  Created by Hahn on 2019/12/2.
+//  Copyright © 2019 Hahn. All rights reserved.
+//
+
+import XCTest
+@testable import Hahn_Pop_Swift
+
+class Hahn_Pop_SwiftTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
